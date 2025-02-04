5ea79b2a107c9dfd090334a89bec300877e04445
AlbionCircus <hello@albioncircus.com>
2025-02-04 16:03:17 +0000

    Jest install + tests

--- app/components/Nav.tsx
+++ app/components/Nav.tsx
@@ -2,7 +2,7 @@ import Link from "next/link"
 
 export default async function Navigation() {
     return (
-        <nav className="mx-auto pt-16 pb-6 flex flex-row items-center justify-between w-64">
+        <nav className="mx-auto pt-16 pb-6 flex flex-row items-center justify-between w-56">
             <Link href={"/"} className="text-slate-700 no-underline hover:underline hover:underline-offset-2">Home</Link>
             <span className="text-green-500"> | </span>
             <Link href={"/products"} className="text-slate-700 no-underline hover:underline hover:underline-offset-2">Products</Link>
